9323042f0a9ca4a2935ebf7064e6ccdb37a41a0e
lokhunliao <lokhunnnn@gmail.com>
2021-06-16 00:57:00 -0700

    5-menu final

--- 5-menu/src/Categories.js
+++ 5-menu/src/Categories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems, categories}) => {
+  return (
+    <div className='btn-container'>
+      {/* <button className='filter-btn' onClick={()=>filterItems('breakfast')}>breakfast</button> */}
+      {categories.map((category, index) => {
+        return <button type='button' className='filter-btn' key={index}
+        onClick={() => filterItems(category)}>
+          {category}
+        </button>
+      })}
+    </div>
+  );
 };
 
 export default Categories;
