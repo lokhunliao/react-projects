316a489691e9f25819d95eff82f254b50113bfce
lokhunliao <lokhunnnn@gmail.com>
2021-06-25 02:07:25 -0700

    15-cocktail final

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio doloremque ab adipisci perferendis non commodi veritatis nemo eveniet omnis. A incidunt repellat ex! Modi numquam obcaecati voluptatum ullam porro nulla facere temporibus eum deleniti, soluta debitis quaerat consectetur quidem iste?</p>
+    </section>
   )
 }
 
